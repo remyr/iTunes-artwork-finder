2d32b2c954520338611c396b60124502f88e9257
Remyr <rivals.remy@yahoo.com>
2016-06-20 18:09:24 +0200

    fix issue with date format

--- src/components/single-result.js
+++ src/components/single-result.js
@@ -1,4 +1,6 @@
 import React, { Component } from 'react'
+import moment from 'moment'
+import 'moment/locale/fr'
 
 class SingleResult extends Component {
 
@@ -9,12 +11,14 @@ class SingleResult extends Component {
     }
 
     render(){
+
+        let time = moment(this.props.result.releaseDate).format('DD MMM YYYY');
         return(
             <tr style={{'cursor': 'pointer'}} onClick={this.HandlerClick.bind(this)}>
                <td><img src={this.props.result.artworkUrl60} alt=""/></td>
                <td>{this.props.result.artistName}</td>
                <td>{this.props.result.collectionName}</td>
-               <td>{this.props.result.releaseDate}</td>
+               <td>{time}</td>
            </tr>
         )
     }
